b5c19238ac398897e5439db3e3bbc46d0d441cca
ALI <fsmaccounting@hotmail.com>
2023-05-15 18:28:56 +0100

    updated Url

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+import axios from 'axios';
+
+
+
+function App() {
+  const url = 'https://course-api.com/react-tours-project';
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchData = async () => {
+    // will use later
+    setLoading(true);
+    try {
+      const { data } = await axios.get(url)
+      setLoading(false);
+      setTours(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+}
+
+export default App;
